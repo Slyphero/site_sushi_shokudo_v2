fd08dfb80bbf74348efcefe7418f52e37c52fa0e
Raphaël Heng <raphael.heng2@gmail.com>
2024-09-29 23:37:25 +0200

    essais nouvelle version site

--- sushi_shokudo/src/App.vue
+++ sushi_shokudo/src/App.vue
@@ -4,12 +4,25 @@ import { RouterLink, RouterView } from 'vue-router'
 
 <template>
   <header>
+    <h1>Sushi Shokudo</h1>
+		<p>Restaurant familial de <span class="accent-text">Sushis</span></p>
   </header>
   <RouterView />
   <footer>
+
   </footer>
 </template>
 
-<style scoped lang="scss">
+<style lang="scss">
+	* {
+		font-family: 'Gabarito';
+	}
 
+	p {
+		font-size: 1.3em;
+	}
+
+	h1, .accent-text {
+		@include color-variation-opacity(color, $primary-color, 1);
+	}
 </style>
--- sushi_shokudo/vite.config.js
+++ sushi_shokudo/vite.config.js
@@ -12,5 +12,17 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
+  },
+
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `
+          @import "src/assets/scss/_variables.scss";
+          @import "src/assets/scss/_mixins.scss";
+          @import url('https://fonts.googleapis.com/css2?family=Gabarito:wght@400..900&display=swap');
+        `
+      }
+    }
   }
 })
