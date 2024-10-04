87b506133c3b2ece4b10549ca9e4b2e57adca36d
Raphaël Heng <raphael.heng2@gmail.com>
2024-10-04 20:56:02 +0200

    ajout sushis

---
+++ sushi_shokudo_ts/src/assets/constants/sushisEntriesConstants.ts
@@ -0,0 +1,85 @@
+import type { FoodEntry } from '../models/menuInterface'
+
+const imageFolderPath: string = 'src/assets/img/'
+
+export const sushisEntries: FoodEntry[] = [
+  {
+    title: 'Sushi avocat',
+    piecesNumber: 2,
+    price: 3,
+    image: imageFolderPath.concat('sushi_shokudo.webp'), 
+    elements: ['avocat', 'feuille de nori']
+  },
+  {
+    title: 'Sushi saumon',
+    piecesNumber: 2,
+    price: 3.9,
+    image: imageFolderPath.concat('sushi_shokudo.webp'), 
+  },
+  {
+    title: 'Sushi saumon snacké',
+    piecesNumber: 2,
+    price: 3.9,
+    image: imageFolderPath.concat('sushi_shokudo.webp'), 
+    elements: ['saumon snacké', 'teriyaki', 'sésame']
+  },
+  {
+    title: 'Sushi saumon cheese',
+    piecesNumber: 2,
+    price: 4.5,
+    image: imageFolderPath.concat('sushi_shokudo.webp'), 
+    elements: ['Cream cheese', '7 épices']
+  },
+  {
+    title: 'Sushi thon',
+    piecesNumber: 2,
+    price: 4.3,
+    image: imageFolderPath.concat('sushi_shokudo.webp'), 
+  },
+  {
+    title: 'Sushi thon spicy',
+    piecesNumber: 2,
+    price: 4.5,
+    image: imageFolderPath.concat('sushi_shokudo.webp'), 
+    elements: ['mayonnaise épicée', 'piment en topping']
+  },
+  {
+    title: 'Sushi thon snacké',
+    piecesNumber: 2,
+    price: 4.5,
+    image: imageFolderPath.concat('sushi_shokudo.webp'), 
+    elements: ['Thon snacké', 'teriyaki', 'sésame']
+  },
+  {
+    title: 'Sushi daurade',
+    piecesNumber: 2,
+    price: 4.3,
+    image: imageFolderPath.concat('sushi_shokudo.webp'), 
+  },
+  {
+    title: 'Sushi crevette Ebi',
+    piecesNumber: 2,
+    price: 4.6,
+    image: imageFolderPath.concat('sushi_shokudo.webp'), 
+    elements: ['Crevette', 'feuille de nori']
+  },
+  {
+    title: 'Sushi oeufs de saumon',
+    piecesNumber: 2,
+    price: 6,
+    image: imageFolderPath.concat('sushi_shokudo.webp'), 
+  },
+  {
+    title: 'Sushi crabe',
+    piecesNumber: 2,
+    price: 6,
+    image: imageFolderPath.concat('sushi_shokudo.webp'), 
+  },
+  {
+    title: 'Sushi tulip ikura saumon',
+    piecesNumber: 3,
+    price: 10.5,
+    image: imageFolderPath.concat('sushi_shokudo.webp'), 
+    elements: ['avocat', 'oeufs de saumon']
+  },
+]
--- sushi_shokudo_ts/src/views/MenuView.vue
+++ sushi_shokudo_ts/src/views/MenuView.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import { menuEntries } from '@/assets/constants/menuEntriesConstants'
+import { sushisEntries } from '@/assets/constants/sushisEntriesConstants'
 
 import MenuEntry from '@/components/MenuEntry.vue'
 
@@ -7,14 +8,18 @@ import MenuEntry from '@/components/MenuEntry.vue'
 
 <template>
 
-<div class="menus-entry-container">
+<div class="food-entry-container">
   <MenuEntry v-for="menuEntry in menuEntries" :foodEntry=menuEntry />
+</div>
+
+<div class="food-entry-container">
+  <MenuEntry v-for="sushiEntry in sushisEntries" :foodEntry=sushiEntry />
 </div>
 
 </template>
 
 <style scoped lang="scss">
-  .menus-entry-container {
+  .food-entry-container {
     display: flex;
     flex-direction: row;
     flex-wrap: wrap;
