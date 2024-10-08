90400072240b7115e76794a2846e245a69825eca
Raphaël Heng <raphael.heng2@gmail.com>
2024-10-08 14:12:13 +0200

    ajout springs

---
+++ sushi_shokudo_ts/src/assets/constants/springsEntriesConstants.ts
@@ -0,0 +1,95 @@
+import type { FoodEntry } from '../models/foodEntryInterface'
+import { imageFolderPath } from "./imageFolderPathConstant";
+
+export const springsEntries: FoodEntry[] = [
+  {
+    title: 'Spring concombre, cheese',
+    piecesNumber: 6,
+    price: 5.5,
+    image: imageFolderPath.concat('sushi_shokudo.webp'),
+  },
+  {
+    title: 'Spring avocat, cheese',
+    piecesNumber: 6, 
+    price: 5.5,
+    image: imageFolderPath.concat('sushi_shokudo.webp'),
+  },
+  {
+    title: 'Spring saumon, avocat',
+    piecesNumber: 6,
+    price: 6.5,
+    image: imageFolderPath.concat('sushi_shokudo.webp'),
+  },
+  {
+    title: 'Spring saumon, cheese',
+    piecesNumber: 6,
+    price: 6.9,
+    image: imageFolderPath.concat('sushi_shokudo.webp'),
+  },
+  {
+    title: 'Spring thon, avocat',
+    piecesNumber: 6,
+    price: 6.8,
+    image: imageFolderPath.concat('sushi_shokudo.webp'),
+  },
+  {
+    title: 'Spring crevette, avocat',
+    piecesNumber: 6,
+    price: 7.3,
+    image: imageFolderPath.concat('sushi_shokudo.webp'),
+  },
+  {
+    title: 'Spring thon cuit, avocat',
+    piecesNumber: 6,
+    price: 6.8,
+    image: imageFolderPath.concat('sushi_shokudo.webp'),
+  },
+  {
+    title: 'Spring Ebi Fry',
+    piecesNumber: 6,
+    price: 7.5,
+    image: imageFolderPath.concat('sushi_shokudo.webp'),
+  },
+  {
+    title: 'Spring végétarien',
+    piecesNumber: 6,
+    price: 6,
+    image: imageFolderPath.concat('sushi_shokudo.webp'),
+  },
+  {
+    title: 'Spring daurade, wasabi',
+    piecesNumber: 6,
+    price: 6.9,
+    image: imageFolderPath.concat('sushi_shokudo.webp'),
+  },
+  {
+    title: 'Spring poulet pané',
+    piecesNumber: 6,
+    price: 6.8,
+    image: imageFolderPath.concat('sushi_shokudo.webp'),
+  },
+  {
+    title: 'Spring tataki, saumon',
+    piecesNumber: 6,
+    price: 7.2,
+    image: imageFolderPath.concat('sushi_shokudo.webp'),
+  },
+  {
+    title: 'Spring tataki, thon',
+    piecesNumber: 6,
+    price: 7.5,
+    image: imageFolderPath.concat('sushi_shokudo.webp'),
+  },
+  {
+    title: 'Spring chili crabe',
+    piecesNumber: 6,
+    price: 7.5,
+    image: imageFolderPath.concat('sushi_shokudo.webp'),
+  },
+  {
+    title: 'Spring thon cuit, concombre',
+    piecesNumber: 6,
+    price: 6.5,
+    image: imageFolderPath.concat('sushi_shokudo.webp'),
+  },
+]
