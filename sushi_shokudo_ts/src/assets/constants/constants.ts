359a7c479aaaf7825307885e6dfc216882a14e01
Raphaël Heng <raphael.heng2@gmail.com>
2024-12-07 00:50:43 +0100

    fix link

--- sushi_shokudo_ts/src/assets/constants/constants.ts
+++ sushi_shokudo_ts/src/assets/constants/constants.ts
@@ -8,7 +8,7 @@ export const mailto: string = "mailto:".concat(email);
 
 export const phone: string = "09 82 78 19 41 / 06 99 03 44 23"
 
-export const uberLink: string = "https://www.ubereats.com/store/sushi-shokudo/nAaJVHvcWCe88ZtaBJGXhA?diningMode=DELIVERY&pl=JTdCJTIyYWRkcmVzcyUyMiUzQSUyMldlbGxpbyUyMEx5b24lMjBQYXJ0LURpZXUlMjIlMkMlMjJyZWZlcmVuY2UlMjIlM0ElMjJDaElKa1p1aHVLanI5RWNSYmpTZWhvZ1hOcW8lMjIlMkMlMjJyZWZlcmVuY2VUeXBlJTIyJTNBJTIyZ29vZ2xlX3BsYWNlcyUyMiUyQyUyMmxhdGl0dWRlJTIyJTNBNDUuNzU5MjcyNCUyQyUyMmxvbmdpdHVkZSUyMiUzQTQuODU1OTEzMDk5OTk5OTk5JTdE&ps=1&utm_source=menu-maker";
+export const uberLink: string = "https://www.ubereats.com/fr/store/sushi-shokudo/nAaJVHvcWCe88ZtaBJGXhA?diningMode=DELIVERY&pl=JTdCJTIyYWRkcmVzcyUyMiUzQSUyMldlbGxpbyUyMEx5b24lMjBQYXJ0LURpZXUlMjIlMkMlMjJyZWZlcmVuY2UlMjIlM0ElMjJDaElKa1p1aHVLanI5RWNSYmpTZWhvZ1hOcW8lMjIlMkMlMjJyZWZlcmVuY2VUeXBlJTIyJTNBJTIyZ29vZ2xlX3BsYWNlcyUyMiUyQyUyMmxhdGl0dWRlJTIyJTNBNDUuNzU5MjcyNCUyQyUyMmxvbmdpdHVkZSUyMiUzQTQuODU1OTEzMDk5OTk5OTk5JTdE&ps=1";
 
 export const instagramLink: string = "https://www.instagram.com/sushi.shokudo.lyon/";
 
